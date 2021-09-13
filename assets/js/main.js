d9e04cb0bba55a04b2871b551841f6a0381d146b
mochisaurus <90163089+mochisaurus@users.noreply.github.com>
2021-09-13 11:12:50 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'おもいで',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'いくさ',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'ハーモニカ',
           // q2: 'かかか',
           // q3: 'ききき',
         }
